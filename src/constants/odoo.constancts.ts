af8157ff5e97fa18e0c65897b4cc8b9cb5a39178
wanjaumbatia <wanjaumbatia@gmail.com>
2023-06-14 06:33:36 +0300

    .

--- src/constants/odoo.constancts.ts
+++ src/constants/odoo.constancts.ts
@@ -1,5 +1,5 @@
 export const odoo_constants = {
-    BASE_URL: 'http://127.0.0.1:8069/api',
+    BASE_URL: 'http://35.154.224.139/api',
     CREATE_CONTACT_URL: '/create_contact',
     PERSONNEL_URL: '/personnel',
     CATEGORIES_URL: '/categories',
